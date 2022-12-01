c8d270eb70d326ce5ce816e7f47f241cbc925396
unknown <uddeshteke007@gmail.com>
2022-12-02 01:27:38 +0530

    Added moduled js for import for BAAS

--- public/app.js
+++ public/app.js
@@ -1,1 +1,26 @@
-// console.log(app);
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: 'AIzaSyA6Rt-ZGMhGxMKXpBj1C7P5ySZeiWeqAOc',
+  authDomain: 'fir-1-fa1fb.firebaseapp.com',
+  projectId: 'fir-1-fa1fb',
+  storageBucket: 'fir-1-fa1fb.appspot.com',
+  messagingSenderId: '547044376145',
+  appId: '1:547044376145:web:f90d1d8b92484cdf504270',
+  measurementId: 'G-BC745TW44C',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+console.log(app);
+const analytics = getAnalytics(app);
+const auth = getAuth(app);
+console.log(auth);
+console.log(app);
